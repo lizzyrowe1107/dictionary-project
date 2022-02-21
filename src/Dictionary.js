58488597221af90e4aec1f18d8fa44c7dd2ac34d
Liz Rowe <83372467+lizzyrowe1107@users.noreply.github.com>
2022-02-20 18:37:44 -0800

    Added Axios and API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
 
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        //console.log(response); to check the response
+
+        //console.log(response.data[0]); after playing around only want first data point
+    }
+
 
     function search(event) {
         event.preventDefault();
 
-        alert(`Searching ${keyword} definition`)
+        
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+
+
+
+        //make request using axios
+
+        axios.get(apiUrl).then(handleResponse)
 
     }
+
+    
 
     function handleKeywordChange(event) {
         //console.log(event) to find where the value is
